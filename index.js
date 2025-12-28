2a557c39ce8d54bc439bd9acb33c8b79fcf65418
hashir <muhammadhashir5889@gmail.com>
2025-12-28 13:43:00 +0500

    bug fix v0.3

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const { PORT, MONGODB_URL } = process.env;
 const port = PORT || 8000;
 const authRouter = require("./Routes/Auth/auth.routes");
 const notesRouter = require("./Routes/Notes/notes.route");
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+// app.use(cors());
 app.options("*", cors());
 app.use(express.json());
 
